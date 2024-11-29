1d388e7a79cca5d0a9cff3d6487a5453b6a8e468
StanV <48187702+TheStanster@users.noreply.github.com>
2024-11-29 19:10:36 +0100

    Implemented algorithm for shortest path calculation.
    Added a priority queue and distance tracking to efficiently compute the shortest path between nodes in a graph. The implementation returns the correct path or an empty array if no path is found.

--- Pathfinder/Algorithm.cs
+++ Pathfinder/Algorithm.cs
@@ -8,7 +8,52 @@ namespace Pathfinder
     {
         public static IEnumerable<Node> ShortestPath(Graph graph, Node from, Node to)
         {
-            return Enumerable.Empty<Node>();
+            var distance = new Dictionary<Node, double>();
+            var previous = new Dictionary<Node, Node>();
+            const double tolerance = 0.0;
+            var priorityQueue = new SortedSet<(double distance, Node node)>(Comparer<(double, Node)>.Create((x, y) => Math.Abs(x.Item1 - y.Item1) > tolerance ? x.Item1.CompareTo(y.Item1) : x.Item2.GetHashCode().CompareTo(y.Item2.GetHashCode())));
+            
+            foreach (var node in graph.Nodes)
+            {
+                distance[node] = double.PositiveInfinity;
+                previous[node] = null;
+            }
+
+            distance[from] = 0;
+            priorityQueue.Add((0, from));
+
+            while (priorityQueue.Any())
+            {
+                var current = priorityQueue.Min.node;
+                priorityQueue.Remove(priorityQueue.Min);
+
+                if (current.Equals(to))
+                {
+                    var path = new List<Node>();
+                    while (previous[current] != null)
+                    {
+                        path.Add(current);
+                        current = previous[current];
+                    }
+                    path.Add(from);
+                    path.Reverse();
+                    return path;
+                }
+
+                foreach (var (weight, neighbor) in graph.Neighbors(current))
+                {
+                    var currentToNeighbor = distance[current] + weight;
+                    if (currentToNeighbor < distance[neighbor])
+                    {
+                        priorityQueue.Remove((distance[neighbor], neighbor));
+                        distance[neighbor] = currentToNeighbor;
+                        previous[neighbor] = current;
+                        priorityQueue.Add((currentToNeighbor, neighbor));
+                    }
+                }
+            }
+
+            return new Node[] { };
         }
     }
 }
